941c718b3d649328929a0720f87160e853064aca
Phil Brocker <phil.brocker@gmail.com>
2025-10-31 23:16:05 -0400

    chore: update core asdf files

--- lib/utils.bash
+++ lib/utils.bash
@@ -8,91 +8,94 @@ TOOL_NAME="protoc-gen-js"
 TOOL_TEST="protoc-gen-js --version"
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
-  exit 1
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
 }
 
 curl_opts=(-fsSL)
 
 # NOTE: You might want to remove this if protoc-gen-js is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+	git ls-remote --tags --refs "$GH_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if protoc-gen-js has other means of determining installable versions.
-  list_github_tags
+	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
+	# Change this function if protoc-gen-js has other means of determining installable versions.
+	list_github_tags
 }
 
 get_platform() {
-  local os=$(uname)
-  if [[ "${os}" == "Darwin" ]]; then
-    echo "osx"
-  elif [[ "${os}" == "Linux" ]]; then
-    echo "linux"
-  else
-    echo >&2 "unsupported os: ${os}" && exit 1
-  fi
+	local os
+	os=$(uname)
+	if [[ "${os}" == "Darwin" ]]; then
+		echo "osx"
+	elif [[ "${os}" == "Linux" ]]; then
+		echo "linux"
+	else
+		echo >&2 "unsupported os: ${os}" && exit 1
+	fi
 }
 
 get_arch() {
-  local os=$(uname)
-  local arch=$(uname -m)
-  # On ARM Macs, uname -m returns "arm64", but in protoc releases this architecture is called "aarch_64"
-  if [[ "${os}" == "Darwin" && "${arch}" == "arm64" ]]; then
-    echo "aarch_64"
-  elif [[ "${os}" == "Linux" && "${arch}" == "aarch64" ]]; then
-    echo "aarch_64"
-  else
-    echo "${arch}"
-  fi
+	local os
+	os=$(uname)
+	local arch
+	arch=$(uname -m)
+	# On ARM Macs, uname -m returns "arm64", but in protoc releases this architecture is called "aarch_64"
+	if [[ "${os}" == "Darwin" && "${arch}" == "arm64" ]]; then
+		echo "aarch_64"
+	elif [[ "${os}" == "Linux" && "${arch}" == "aarch64" ]]; then
+		echo "aarch_64"
+	else
+		echo "${arch}"
+	fi
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+	local version filename url
+	version="$1"
+	filename="$2"
 
-  url="$GH_REPO/releases/download/v${version}/protobuf-javascript-${version}-$(get_platform)-$(get_arch).tar.gz"
+	url="$GH_REPO/releases/download/v${version}/protobuf-javascript-${version}-$(get_platform)-$(get_arch).tar.gz"
 
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	echo "* Downloading $TOOL_NAME release $version..."
+	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-  local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+	local install_type="$1"
+	local version="$2"
+	local install_path="${3%/bin}/bin"
 
-  if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
-  fi
+	if [ "$install_type" != "version" ]; then
+		fail "asdf-$TOOL_NAME supports release installs only"
+	fi
 
-  (
-    mkdir -p "$install_path"
-    cp "$ASDF_DOWNLOAD_PATH/bin"/* "$install_path/$TOOL_NAME"
+	(
+		mkdir -p "$install_path"
+		cp "$ASDF_DOWNLOAD_PATH/bin"/* "$install_path/$TOOL_NAME"
 
-    # TODO: Assert protoc-gen-js executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		# TODO: Assert protoc-gen-js executable exists.
+		local tool_cmd
+		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-    echo "$TOOL_NAME $version installation was successful!"
-    rm -rf "$ASDF_DOWNLOAD_PATH"/*
-  ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
-  )
+		echo "$TOOL_NAME $version installation was successful!"
+		rm -rf "${ASDF_DOWNLOAD_PATH:?}"/*
+	) || (
+		rm -rf "$install_path"
+		fail "An error occurred while installing $TOOL_NAME $version."
+	)
 }
